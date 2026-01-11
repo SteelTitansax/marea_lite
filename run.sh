32a65a7951c9a8903a7d9d40c728acffd76dff0a
SteelTitansax <112019917+SteelTitansax@users.noreply.github.com>
2026-01-11 10:26:39 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # Activate venv and run the initial command
-source /home/titansax/personal_agent/venv/bin/activate
+source /home/titansax/marea_lite/venv/bin/activate
 
-python3 /home/titansax/personal_agent/chatbot.py
+python3 /home/titansax/marea_lite/chatbot.py
